f02b3a804cb27d3c6d42296e0ecfb54d8b84e5fd
valeriobartoletti97 <valerio.bartoletti97gmail.com>
2023-10-15 22:31:12 +0200

    working on second exercise

---
+++ array-tail/js/script.js
@@ -0,0 +1,15 @@
+let resultEl = document.querySelector('.d-none');
+const buttonEl = document.querySelector('.btn');
+let data = document.getAnimations('data').value;
+
+buttonEl.addEventListener('click', function(){
+   
+})
+
+
+/* utility */
+
+// utility
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
