83a079c40c4aeb2e1b2fa0bf73ae74eb56fb9527
valeriobartoletti97 <valerio.bartoletti97gmail.com>
2023-10-15 22:15:04 +0200

    first snack finished

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,1 +1,20 @@
-let fridgeFruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
+let fridgeFruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomeo', 'limone', 'fragola'];
+fridgeFruits.push('apple');
+console.log(fridgeFruits);
+let resultEl = document.querySelector('.d-none');
+let present = false;
+
+for (let i = 0; i <= fridgeFruits.length; i++){
+   if (fridgeFruits[i] === 'cocomero'){
+    present = true
+   }
+};
+resultEl.classList.remove('d-none')
+if (present){
+    resultEl.innerHTML ='Trovato! Devo solo preparare il cocktail.';
+    resultEl.className= 'bg-success';
+} else {
+    resultEl.className = 'bg-danger';
+    resultEl.innerHTML = 'Oh no, devo uscire a comprare il cocomero! '
+}
+
